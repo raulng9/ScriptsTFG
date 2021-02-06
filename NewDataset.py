e7581d48b7e6595c676da9830250c583cf3ddede
Raúl Núñez García <salprez4@gmail.com>
2021-02-06 22:26:55 +0100

    Started calculations of extra coordinates parameters

--- NewDataset.py
+++ NewDataset.py
@@ -41,35 +41,35 @@ class Dataset(torch.utils.data.Dataset):
         indexForPatches = imageToPatch.imageIndex
         actualImage = imageToPatch.imageContent
         channels = dimensionsOfImage[2]
+        #   If the size of the current image is already small enough for our GPU
+        #   we can create the patch from it
         if width * height < pow(self.maxDimensionsForGPU, 2):
-            newPatch = Patch(imageToPatch.imageContent,imageToPatch.imageIndex,[imageToPatch.topLeftCoordinates,imageToPatch.bottomRightCoordinates],1)
-            #   Append to list of patches
+            #   Create patch and calculate all the coordinates for its parameters
+            newPatch = Patch(imageToPatch.imageContent,imageToPatch.imageIndex,[imageToPatch.topLeftCoordinates,imageToPatch.bottomRightCoordinates],lengthOfHalo)
+            newPatch.set_absolute_image_shape(width,height)
+            newPatch.calculate_extra_coordinates()
+            #   Add new patch to the general list
             self.listOfPatches.append(newPatch)
+            #   Draw rectangles representing the chopping for testing purposes
             cv2.rectangle(self.imageCopyForRects, (int(imageToPatch.topLeftCoordinates[0]),int(imageToPatch.topLeftCoordinates[1])), (int(imageToPatch.bottomRightCoordinates[0]),int(imageToPatch.bottomRightCoordinates[1])), (255,0,0), 10)
             print(newPatch.coordinatesInOriginalImage)
             print("------------------------")
-
         else:
             #   Computation of the coordinates of the patches with respect to the image
             firstPatchArea = actualImage[0:int(0+height/2+lengthOfHalo), 0:int(0+width/2+lengthOfHalo)]
             coordinatesFirstPatch = [[imageToPatch.topLeftCoordinates[0],imageToPatch.topLeftCoordinates[1]], [imageToPatch.topLeftCoordinates[0] + width/2 +lengthOfHalo,imageToPatch.topLeftCoordinates[1] + height/2 + lengthOfHalo]]
-
             secondPatchArea = actualImage[0:int(0+height/2+lengthOfHalo), int(0+width/2-lengthOfHalo):width]
             coordinatesSecondPatch = [[imageToPatch.topLeftCoordinates[0]+width/2-lengthOfHalo,imageToPatch.topLeftCoordinates[1]], [imageToPatch.bottomRightCoordinates[0],imageToPatch.topLeftCoordinates[1] + height/2 + lengthOfHalo]]
-
             thirdPatchArea = actualImage[int(height/2-lengthOfHalo):height, 0:int(width/2+lengthOfHalo)]
             coordinatesThirdPatch = [[imageToPatch.topLeftCoordinates[0],imageToPatch.topLeftCoordinates[1] + height/2 -lengthOfHalo], [imageToPatch.topLeftCoordinates[0]+width/2+lengthOfHalo,imageToPatch.bottomRightCoordinates[1]]]
-
             fourthPatchArea = actualImage[int(height/2-lengthOfHalo):height, int(width/2-lengthOfHalo):width]
             coordinatesFourthPatch = [[imageToPatch.topLeftCoordinates[0]+width/2-lengthOfHalo,imageToPatch.topLeftCoordinates[1] + height/2 -lengthOfHalo], [imageToPatch.bottomRightCoordinates[0],imageToPatch.bottomRightCoordinates[1]]]
-
 
             #   Creation of images for possible further cropping
             firstImage = Image(indexForPatches,firstPatchArea,coordinatesFirstPatch[0],coordinatesFirstPatch[1])
             secondImage = Image(indexForPatches,secondPatchArea,coordinatesSecondPatch[0],coordinatesSecondPatch[1])
             thirdImage = Image(indexForPatches,thirdPatchArea,coordinatesThirdPatch[0],coordinatesThirdPatch[1])
             fourthImage = Image(indexForPatches,fourthPatchArea,coordinatesFourthPatch[0],coordinatesFourthPatch[1])
-
 
             #   Recursive call for further chopping if necessary
             self.get_patches_from_image(firstImage,self.HALO_SIZE)
@@ -104,15 +104,14 @@ class Dataset(torch.utils.data.Dataset):
         #cv2.imwrite(saveName, self.imageCopyForRects)
 
     def load_ground_truth(self, imageForGroundTruth):
-        print("Ground truth for input:")
+        print("Loading ground truth")
         counterGTFiles = 0
-
         for root, dirs, files in os.walk(self.groundtruth_path):
              for imageGT in files:
                  if imageGT.endswith('.png') and imageGT == imageForGroundTruth:
-                     print(imageGT)
+                     #print(imageGT)
                      counterGTFiles += 1
-        print(counterGTFiles)
+        print("Loaded ground truth with " + str(counterGTFiles) + " files")
         print("-----------------------")
 
 
@@ -126,7 +125,7 @@ class Dataset(torch.utils.data.Dataset):
                 print(fileInDataset)
                 self.load_ground_truth(fileInDataset)
                 counterInputFiles += 1
-        #print(counterInputFiles)
+        print(str(counterInputFiles) + " images have been loaded, along with their corresponding ground truths")
 
 
     def create_dataset(self, input_path, groundtruth_path=None):
@@ -134,7 +133,7 @@ class Dataset(torch.utils.data.Dataset):
         self.groundtruth_path = groundtruth_path
         self.files = []
         self.load_input(input_path)
-        self.load_ground_truth(groundtruth_path)
+        #self.load_ground_truth(groundtruth_path)
         # print("starting")
         # imagePaths = glob.glob("*.png")
         # for image in imagePaths:
--- Patch.py
+++ Patch.py
@@ -8,11 +8,70 @@ class Patch():
     patchImage = None
     indexOfImage = None
     coordinatesInOriginalImage = None
-    coordinatesWithoutHalo = None
-    #extra param which I don't yet understand
+    relativeCoordinatesWithoutHalo = None
+    coordinatesInOriginalImageWithoutHalo = None
 
-    def __init__(self, imageForPatch, index, coordinatesOriginal, coordinatesWithoutHalo):
+
+    topLeftCoordinate = None
+    topRightCoordinate = None
+    bottomLeftCoordinate = None
+    bottomRightCoordinate = None
+
+    #   Patch dimensions for relative coordinates calculation
+    patchWidth = None
+    patchHeight = None
+
+    #   Sides that have a halo include: Top right bottom left
+    sidesWithHalo = [True,True,True,True]
+    haloSize = None
+
+    #   Variables for calculating the halo substractions
+    widthOfMainImage = None
+    heightOfMainImage = None
+
+
+    def calculate_initial_relative_coordinates(self):
+        self.topLeftCoordinate = [0,0]
+        self.topRightCoordinate = [self.patchWidth,0]
+        self.bottomLeftCoordinate = [0,self.patchHeight]
+        self.bottomRightCoordinate = [self.patchWidth,self.patchHeight]
+
+    def __init__(self, imageForPatch, index, coordinatesOriginal,haloSizeUsed):
         self.patchImage = imageForPatch
         self.indexOfImage = index
         self.coordinatesInOriginalImage = coordinatesOriginal
-        self.coordinatesWithoutHalo = coordinatesWithoutHalo
+        self.haloSize = haloSize
+        patchDimensions = imageForPatch.shape
+        self.patchWidth = patchDimensions[1]
+        self.patchHeight = patchDimensions[0]
+        self.calculate_initial_relative_coordinates()
+
+    def set_absolute_image_shape(self, widthOfGeneralImage, heightOfGeneralImage):
+        self.widthOfMainImage = widthOfGeneralImage
+        self.heightOfMainImage = heightOfGeneralImage
+
+    def calculate_relative_coordinates(self):
+        if self.sidesWithHalo[0] == False:
+            self.topLeftCoordinate = [0,0]
+        if self.sidesWithHalo[1] == False:
+            self.topRightCoordinate = [self.patchWidth,0]
+        if self.sidesWithHalo[2]
+        return
+
+    def calculate_absolute_coordinates_without_halo(self):
+        return
+
+    def calculate_halo_sides(self):
+        if self.coordinatesInOriginalImage[0][0] == 0:
+            sidesWithHalo[-1] = False
+        if self.coordinatesInOriginalImage[0][1] == 0:
+            sidesWithHalo[0] = False
+        if self.coordinatesInOriginalImage[1][0] == widthOfMainImage:
+            sidesWithHalo[1] = False
+        if self.coordinatesInOriginalImage[1][1] == heightOfMainImage:
+            sidesWithHalo[2] = False
+
+    def calculate_extra_coordinates(self):
+        self.calculate_halo_sides()
+        self.calculate_relative_coordinates()
+        self.calculate_absolute_coordinates_without_halo()
